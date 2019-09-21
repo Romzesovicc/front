443824957f7f7caef584075ea00a2f41c61974bb
Romzesovicc <romasha1989bezzon@gmail.com>
2019-09-21 16:39:57 +0300

    Minor improvements;

--- script/scroll.js
+++ script/scroll.js
@@ -1,24 +1,21 @@
  document.body.onscroll = function(){
     let scrollValue = document.documentElement.scrollTop;
     let previousScroll = 60;
-    let show_text = 250;
-    let text_hide = 670;
+    let text_dimensions = document.getElementsByClassName('center2')[0].getBoundingClientRect();
+    const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
     //check in PC mode
 
     if(scrollValue > previousScroll) {
-        console.log(window.scrollY);
         document.querySelector('.menu-block').classList.add('menu-nav_remove');
     } else
         document.querySelector('.menu-block').classList.remove('menu-nav_remove');
-       if (scrollValue > show_text)
-           document.getElementById('text_show').classList.add('text_show');
-       else
-           document.getElementById('text_show').classList.remove('text_show');
-       if (scrollValue > text_hide)  //new items
-           document.getElementById('text_show').classList.add('text_hide');
-        else
-          document.getElementById('text_show').classList.remove('text_hide');
+       if (scrollValue + viewportHeight > text_dimensions.top + text_dimensions.height) {
+         document.getElementById('text_show').classList.add('text_show');
+         setTimeout(() => {
+           document.getElementById('text_show').style.visibility = 'visible';
+         }, 500);
+       }
 
      //check in mobile mode
 
