29d4a8d2b79e248f02e89fa25b6127bda7ea7f3d
Romzesovicc <romasha1989bezzon@gmail.com>
2019-09-25 20:03:46 +0300

    Sound fix;

--- script/sound_playing.js
+++ script/sound_playing.js
@@ -1,7 +1,11 @@
 function sound_playing(){
     let play = document.querySelector('.playing');
+    let audio = document.getElementById('mySound');
     if(play) {
+        audio.pause();
         document.getElementById('sound').classList.remove('playing');
-    }else
+    } else {
+        audio.play();
         document.getElementById('sound').classList.add('playing');
-    };
+    }
+}
